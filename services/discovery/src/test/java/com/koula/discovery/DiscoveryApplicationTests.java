32cbd6b7e5d55cfb37e7eb405d58b27a50f3a773
abdiallo <abdiallo@gainde2000.sn>
2025-06-06 20:59:29 +0200

    feat(discovery-service): add eureka server for service registration and discovery

---
+++ services/discovery/src/test/java/com/koula/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koula.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
